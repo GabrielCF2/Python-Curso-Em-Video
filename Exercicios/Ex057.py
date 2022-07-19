ab8abb1e102482e7b8566880339cc411d856b81a
GabrielCF <gabrielcotrimbatata@gmail.com>
2022-07-19 16:10:04 -0300

    metodo strip adicionado

--- Exercicios/Ex057.py
+++ Exercicios/Ex057.py
@@ -1,6 +1,6 @@
 #Faça um programa que leia o sexo de uma pessoa, mas só aceite valores 'M' ou 'F'.
 #Caso esteja errado faça a digitação novamente até ter um valor correto.
-sexo = str(input("Digite o seu sexo: ").upper())
+sexo = str(input("Digite o seu sexo: ")).strip().upper()[0]
 while sexo != 'M' and sexo != 'F':
     sexo = str(input(f"Você digitou {sexo}, que é um valor inválido, digite novamene: ").upper())
 print(f"Você escolheu o sexo {sexo}.")
